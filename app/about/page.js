c71064011be3dffa3284e18ce768efba8c7a94da
Ankush2107 <ankush.kumar210701@gmail.com>
2024-05-18 09:50:42 +0530

    file based routing

---
+++ app/about/page.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const about = () => {
+  return (
+    <div>
+        <h1 className='text-xl'>Problem solved by Next.js</h1>
+        <ul>
+          <l1>Full Stack Solution</l1>
+          <l1>File based routing</l1>
+          <l1>Additional feature like router from next/navigation</l1>
+          <l1>Optimized rendering</l1>
+        </ul>
+    </div>
+  )
+}
+
+export default about;
